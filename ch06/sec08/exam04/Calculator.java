9e070e244daefc3db6c08799346c8bedb694cce6
Neon <sj060700@naver.com>
2024-10-18 11:31:51 +0900

    Add files via upload

---
+++ ch06/sec08/exam04/Calculator.java
@@ -0,0 +1,13 @@
+package ch06.sec08.exam04;
+
+public class Calculator {
+
+	double areaRectangle(double width) {
+		return width * width;
+	}
+	
+	
+	double areaRectangle(double width, double height) {
+		return width * height;
+	}
+}

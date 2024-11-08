88e98e47956b6d88f945f5c0c284ed10e64483dd
Neon <sj060700@naver.com>
2024-11-08 13:17:56 +0900

    Add files via upload

---
+++ ch07/sec10/exam01/PhoneExample.java
@@ -0,0 +1,13 @@
+package ch07.sec10.exam01;
+
+public class PhoneExample {
+	public static void main(String[] args) {
+		//Phone phone = new Phone();
+		
+		SmartPhone smartphone = new SmartPhone("ȫ�浿");
+		
+		smartPhone.turnOn();
+		smartPhone.internetSearch();
+		smartPhone.turnOff();
+	}
+}

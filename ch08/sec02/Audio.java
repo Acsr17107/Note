0c5ea88320cd064c3bc997d7540585aba5dfdb4c
Neon <sj060700@naver.com>
2024-11-16 23:57:40 +0900

    Delete ch08/sec02 directory

--- ch08/sec02/Audio.java
+++
@@ -1,8 +0,0 @@
-package ch08.sec02;
-
-public class Audio implements RemoteControl {
-	@Override
-	public void turnOn() {
-		System.out.println("Audio¸¦ ÄÕ´Ï´Ù.");
-	}
-}
--- ch08/sec02/Television.java
+++
@@ -1,8 +0,0 @@
-package ch08.sec02;
-
-public class Television implements RemoteControl {
-	@Override
-	public void turnOn() {
-		System.out.println("TV¸¦ ÄÕ´Ï´Ù.");
-	}
-}

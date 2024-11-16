287e093c7a3ae719fe2d1530c0f299969254896e
Neon <sj060700@naver.com>
2024-11-16 23:57:47 +0900

    Delete ch08/sec08 directory

--- ch08/sec08/RemoteControl.java
+++
@@ -1,7 +0,0 @@
-package ch08.sec08;
-
-public interface RemoteControl {
-	//추상 메소드
-	void turnOn();
-	void turnOff();
-}
--- ch08/sec08/Searchable.java
+++
@@ -1,6 +0,0 @@
-package ch08.sec08;
-
-public interface Searchable {
-	//추상 메소드
-	void search(String url);
-}

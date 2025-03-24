15587bb3117176e1ad1b61730e6ba334606d55bd
Neon <sj060700@naver.com>
2025-03-24 15:04:17 +0900

    Add files via upload

---
+++ pro0324/Automobile.java
@@ -0,0 +1,27 @@
+package pro0324;
+
+public class Automobile extends Car {
+	//1.속성
+	private int seatNum;
+	
+	//2.생성자
+	Automobile() {} //2-1 기본생성자
+	Automobile(String color, int seatNum, int speed) { //2-2 초기값 만드는 생성자
+		super(color, speed);
+		this.seatNum = seatNum;
+	}
+	
+	//3.할일, 메소드
+	//Automobile 속도올리기
+	void upSpeed(int value) {
+		speed = speed + value;
+		if (speed > 300)
+			speed = 300;
+	}
+	public int getSeatNum() {
+		return seatNum;
+	}
+	public void setSeatNum(int seatNum) {
+		this.seatNum = seatNum;
+	}
+}
---
+++ pro0324/Eagle.java
@@ -0,0 +1,75 @@
+package pro0324;
+
+abstract class Animal {
+	//1. 속성
+	protected String name;
+	
+	//2. 생성자
+	Animal() {}
+	Animal(String name) {
+		this.name = name;
+	}
+	
+	//3. 메소드
+	//abstract
+	abstract void move();
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+
+
+
+public class Eagle extends Animal {
+	//1. 속성
+	private String home;
+	
+	//2. 생성자
+	Eagle() {}
+	Eagle(String name, String home) {
+		super(name);
+		this.home = home;
+	}
+	
+	//3. 메소드
+	@Override
+	void move() {
+		System.out.println("날개로 날아간다.");	
+	}
+	public String getHome() {
+		return home;
+	}
+	public void setHome(String home) {
+		this.home = home;
+	}
+}
+
+
+class tiger extends Animal {
+	//1. 속성
+	private int age;
+	
+	//2. 생성자
+	tiger() {}
+	tiger(String name, String home, int age) {
+		super(name);
+		this.age = age;
+	}
+	
+	//3. 메소드
+	@Override
+	void move() {
+		System.out.println("네 발로 이동한다.");
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
+
